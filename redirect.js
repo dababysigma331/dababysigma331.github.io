477520e225995f0156b8e5684740d7600c32fd19
dababysigma331 <150639142+dababysigma331@users.noreply.github.com>
2024-03-28 17:43:55 -0400

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,59 @@
+function uscap() {
+  window.location.href = "https://dababysigma331.github.io/sites/capitol";
+}
+
+function wh() {
+  window.location.href = "https://dababysigma331.github.io/sites/white";
+}
+
+function supc() {
+  window.location.href = "https://dababysigma331.github.io/sites/supreme";
+}
+
+function natm() {
+  window.location.href = "https://dababysigma331.github.io/sites/national";
+}
+
+function smith() {
+  window.location.href = "https://dababysigma331.github.io/sites/smith";
+}
+
+function wash() {
+  window.location.href = "https://dababysigma331.github.io/sites/wash";
+}
+
+function lincoln() {
+  window.location.href = "https://dababysigma331.github.io/sites/lincoln";
+}
+
+function ford() {
+  window.location.href = "https://dababysigma331.github.io/sites/ford";
+}
+
+function jeff() {
+  window.location.href = "https://dababysigma331.github.io/sites/jeff";
+}
+
+function mlk() {
+  window.location.href = "https://dababysigma331.github.io/sites/mlk";
+}
+
+function viet() {
+  window.location.href = "https://dababysigma331.github.io/sites/vietnam";
+}
+
+function fiveside() {
+  window.location.href = "https://dababysigma331.github.io/sites/pent";
+}
+
+function tomb() {
+  window.location.href = "https://dababysigma331.github.io/sites/tomb";
+}
+
+function arl() {
+  window.location.href = "https://dababysigma331.github.io/sites/arl";
+}
+
+function holo() {
+  window.location.href = "https://dababysigma331.github.io/sites/holo";
+}
